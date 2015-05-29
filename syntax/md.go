d13ef477653b99f0b8ce1fdb4d667f4772afcf46
Thibaut Colar <tcolar@colar.net>
2015-05-28 21:32:23 -0800

    Markdown syntax highlighter

---
+++ syntax/md.go
@@ -0,0 +1,18 @@
+package syntax
+
+var syntaxMarkdown = syntax{
+	Extensions: []string{".md"},
+	Patterns: []SyntaxPattern{
+		NewSyntaxPattern("```", "```", "", true, StyleKw2).WithMSL(), //Code
+		NewSyntaxPattern("`", "`", "", false, StyleKw2),              //Code
+		NewSyntaxPattern("    ", "", "", false, StyleKw2).WithMSL(),  //Code
+		NewSyntaxPattern("#", "", "", false, StyleString).WithMSL(),  //Header
+		NewSyntaxPattern("=", "", "", false, StyleString).WithMSL(),  //Header
+		NewSyntaxPattern("---", "", "", false, StyleKw1).WithMSL(),   //HR
+		NewSyntaxPattern("***", "", "", false, StyleKw1).WithMSL(),   //HR
+		NewSyntaxPattern("___", "", "", false, StyleKw1).WithMSL(),   //HR
+		NewSyntaxPattern(">", "", "", false, StyleComment).WithMSL(), //BlockQuote
+		NewSyntaxPattern("**", "**", "", true, StyleSymb2),           //Bold
+		NewSyntaxPattern("__", "__", "", true, StyleSymb2),           //Bold
+	},
+}
