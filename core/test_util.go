87ea041a6382f50e8d246da6cf6578ef5c392cff
Thibaut Colar <tcolar@colar.net>
2015-08-07 21:18:02 -0700

    Test utils

---
+++ core/test_util.go
@@ -0,0 +1,12 @@
+package core
+
+import "math/rand"
+
+func RandString(n int) string {
+	runes := []rune(" \n\tabcdef\n\t123456\n\t")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = runes[rand.Int63()%int64(len(runes))]
+	}
+	return string(b)
+}
