9356321d0631186e1cff38ffb0b62fc5ef9144c8
Thibaut Colar <tcolar@colar.net>
2015-09-19 11:09:19 -0700

    Terminal exec

---
+++ ui/exec.go
@@ -0,0 +1,36 @@
+package ui
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tcolar/goed/backend"
+	"github.com/tcolar/goed/core"
+)
+
+func exec(args []string) int64 {
+	workDir := "."
+	ed := core.Ed.(*Editor)
+	if ed.CurView() != nil {
+		workDir = ed.CurView().WorkDir()
+	}
+	v := ed.AddViewSmart()
+	v.SetViewType(core.ViewTypeInteractive)
+	b, err := backend.NewMemBackendCmd(args, workDir, v.Id(), nil)
+	if err != nil {
+		ed.SetStatusErr(err.Error())
+	}
+	v.backend = b
+	return v.Id()
+}
+
+func execTerm(args []string) int64 {
+	vid := exec(args)
+	v := core.Ed.ViewById(vid).(*View)
+	b := v.backend.(*backend.BackendCmd)
+	b.IsTerm = true
+	time.Sleep(500 * time.Millisecond)
+	cmd := fmt.Sprintf("source ~/.goed/default/actions/goed.sh %d %d\n", core.InstanceId, v.Id())
+	b.SendBytes([]byte(cmd))
+	return vid
+}
