1beca3d7bab766f137430c28ac63d4d07debbef6
Thibaut Colar <tcolar@colar.net>
2016-01-16 20:15:03 -0800

    Goed shell script fix

--- res/default/actions/goed.sh
+++ res/default/actions/goed.sh
@@ -6,22 +6,17 @@ export GOED_VIEW=$2
 # Search goed custom/builtin tools first 
 export PATH=$HOME/.goed/actions/:$HOME/.goed/default/actions/:$PATH
 
-function goed_open() {
-# open a file in goed
-	goed_api open $GOED_INSTANCE "`pwd`" "$1"
-}
-
 function goed_cd() {
 # cd into a directory, and notify goed of the new dir
 	builtin cd $@
 	goed_api view_cwd $GOED_INSTANCE $GOED_VIEW "`pwd`"
 } 
 
-export EDITOR="goed_open" # When within goed, $EDITOR is goed
+export EDITOR="goed_api open $GOED_INSTANCE `pwd`" # When within goed, $EDITOR is goed
 
 alias cd="goed_cd"
 
-alias o="goed_open" # open a file/dir
+alias o=$EDITOR # open a file/dir
 alias s="s.sh" # search text (=~ grep) 
 alias f="f.sh" # search files (=~ find)
 
