309016de76ad6c8074ca5dda96681d5146f3944f
Thibaut Colar <tcolar@colar.net>
2016-04-01 22:34:00 -0700

    Action to check if command bar is enabled

--- actions/actions_cmd.go
+++ actions/actions_cmd.go
@@ -7,6 +7,13 @@ func (a *ar) CmdbarEnable(on bool) {
 	d(cmdbarEnable{on: on})
 }
 
+// check if cmdbar is enabled
+func (a *ar) CmdbarEnabled() bool {
+	answer := make(chan bool, 1)
+	d(cmdbarEnabled{answer: answer})
+	return <-answer
+}
+
 // Toggle the command bar active or not
 func (a *ar) CmdbarToggle() {
 	d(cmdbarToggle{})
@@ -20,6 +27,14 @@ type cmdbarEnable struct {
 
 func (a cmdbarEnable) Run() {
 	core.Ed.SetCmdOn(a.on)
+}
+
+type cmdbarEnabled struct {
+	answer chan bool
+}
+
+func (a cmdbarEnabled) Run() {
+	a.answer <- core.Ed.CmdOn()
 }
 
 type cmdbarToggle struct{}
